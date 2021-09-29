57c75cefefde99f890986163db7adf65b89672ea
griddhaluru sowmya <sowmyag200100@gmail.com>
2021-09-29 18:52:21 +0530

    junit added

--- java/com/cg/otms/repositories/IAdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cg.otms.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.otms.entities.Admin;
-
-public interface IAdminRepository extends JpaRepository<Admin, Long> {
-}
